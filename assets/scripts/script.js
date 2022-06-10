11a0aabbf57b267ee0de54a7b0db9178ce0d69fc
jessica-calderon <calderonjessica13@yahoo.com>
2022-06-10 03:41:14 -0500

    add search city functionality, local storage, and air quality api

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,52 @@
 // display todays current date on page
 var today = moment().format('MMMM Do, YYYY');
 $("#currentDate").html("<i class='fa-style fa-solid fa-calendar-day'></i>" + today);
+
+//  air quality open data platform api logic
+
+function searchAirQuality(city) {
+    var aqApiKey = "a53b18a6ff2b8d358de6c248c203cfc1ba838e36";
+    var apiUrl = "https://api.waqi.info/feed/" + city + "/?token=" + aqApiKey;
+
+    $.ajax({
+        url: apiUrl,
+        method: 'GET'
+    })
+    .then(function (response) {
+        $("#airQuality").empty();
+
+        // city html section and forecast elements //
+  //      var cityEl = $("<h2>").text(response.name);
+        var air = $("<p>").text(" " + response.data.aqi);
+        var newDivEl = $('<div>')
+        newDivEl.append(air);
+        $("#airQuality").html(newDivEl);
+    });
+}
+/* city location logic */
+$("#searchBtn").on("click", function(event) {
+    event.preventDefault();
+    // set and save searched city variable to localstorage //
+    var citySearch = $("#citySearch").val().trim();
+    var savedSearch = $(this).siblings("input").val();
+    var saved = [];
+    saved.push(savedSearch);
+    localStorage.setItem('city', JSON.stringify(saved));
+
+    searchAirQuality(citySearch);
+    loadSaved();
+});
+/* load localStorage and display on page */
+function loadSaved () {
+    var searchedItem = JSON.parse(localStorage.getItem('city'));
+    var savedSection = $("<button class='styled-btn btn'>").text(searchedItem);
+    var divEl = $("<div>");
+    divEl.append(savedSection)
+    $("#locationSearch").prepend(divEl);
+    
+}
+// clickable location history
+$("#locationHistory").on('click', '.btn', function(event) {
+    event.preventDefault();
+    searchAirQuality($(this).text());
+});
