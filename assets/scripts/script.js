8e761b98e70d1eef0bd1093c041c0cd6f47cb8a3
Jokernal <jordankernzx@gmail.com>
2022-06-11 14:20:57 -0500

    local storage is working like a charm

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,7 +4,7 @@ $("#currentDate").html("<i class='fa-style fa-solid fa-calendar-day'></i>" + tod
 
 // turn searched city into lat/lon for pollen api purposes 
 
-function searchCoord(city) { 
+function searchCoord(city) {
     var searched = JSON.parse(localStorage.getItem('city'));
     var owApiKey = "8146fc372939ba1529f0cee4a074681a";
     var owApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searched + "&units=imperial&appid=" + owApiKey;
@@ -14,28 +14,28 @@ function searchCoord(city) {
         url: owApiUrl,
         method: 'GET'
     })
-    .then(function (response) {
-        console.log(response);
-        var lat = response.coord.lat;
-        var lon = response.coord.lon;
+        .then(function (response) {
+            console.log(response);
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
 
 
-// tomorrow api  
+            // tomorrow api  
 
-    var tomApiKey = "4KmRdN5z9dqbcv6PQ9857fEpkG1PShyN";
-    var treeIndexUrl = "https://api.tomorrow.io/v4/timelines?location=" + lat + "," + lon + "&timesteps=current&units=imperial&apikey=" + tomApiKey + "&fields=treeIndex,grassIndex,weedIndex";
-    $.ajax({
-    url: treeIndexUrl,
-    method: 'GET'
-     })
+            var tomApiKey = "4KmRdN5z9dqbcv6PQ9857fEpkG1PShyN";
+            var treeIndexUrl = "https://api.tomorrow.io/v4/timelines?location=" + lat + "," + lon + "&timesteps=current&units=imperial&apikey=" + tomApiKey + "&fields=treeIndex,grassIndex,weedIndex";
+            $.ajax({
+                url: treeIndexUrl,
+                method: 'GET'
+            })
 
-.then(function (response) {
-    $('#pollenLevels').empty();
-    var uv = response.value;
-    var pollenDiv = $('<p class="pollen-index">').html("<h5>Pollen Index:</h5> " + "<div class='box has-text-light has-background-grey-dark'><i class='fa-style fa-color fa-margin fa-solid fa-seedling'></i> Grass: " + response.data.timelines[0].intervals[0].values.grassIndex + "<br> <i class='fa-style fa-color fa-margin fa-solid fa-tree'></i> Tree: " + response.data.timelines[0].intervals[0].values.treeIndex + "<br> <i class='fa-style fa-color fa-margin fa-brands fa-pagelines'></i> Weed: " + response.data.timelines[0].intervals[0].values.weedIndex + "</p>");
-    $('#pollenLevels').html(pollenDiv)
-})
-})
+                .then(function (response) {
+                    $('#pollenLevels').empty();
+                    var uv = response.value;
+                    var pollenDiv = $('<p class="pollen-index">').html("<h5>Pollen Index:</h5> " + "<div class='box has-text-light has-background-grey-dark'><i class='fa-style fa-color fa-margin fa-solid fa-seedling'></i> Grass: " + response.data.timelines[0].intervals[0].values.grassIndex + "<br> <i class='fa-style fa-color fa-margin fa-solid fa-tree'></i> Tree: " + response.data.timelines[0].intervals[0].values.treeIndex + "<br> <i class='fa-style fa-color fa-margin fa-brands fa-pagelines'></i> Weed: " + response.data.timelines[0].intervals[0].values.weedIndex + "</p>");
+                    $('#pollenLevels').html(pollenDiv)
+                })
+        })
 }
 
 //  air quality open data platform api logic
@@ -48,69 +48,89 @@ function searchAirQuality(city) {
         url: apiUrl,
         method: 'GET'
     })
-    .then(function (response) {
-        $("#airQuality").empty();
-        var air = $("<p>").text("Air Quality Index (AQI): " );
-        var aqi = $("<span>") 
-        aqi.text(response.data.aqi)
-        air.append(aqi)
-        var newDivEl = $('<div>')
-        if(response.data.aqi <= 50 && response.data.aqi >= 0 ) {
-            aqi.css("background-color","green")
-            aqi.css ("color", "white")   
-        } else if (response.data.aqi >= 51 && response.data.aqi <= 100 ) {
-            aqi.css("background-color","yellow")
-            aqi.css ("color", "black")   
-        } else if (response.data.aqi >= 101 && response.data.aqi <= 150 ) {
-            aqi.css("background-color","orange")
-            aqi.css ("color", "black")   
-        } else if (response.data.aqi >= 151 && response.data.aqi <= 200 ) {
-            aqi.css("background-color","red")
-            aqi.css ("color", "white")   
-        }
-        
-        newDivEl.append(air);
-        $("#airQuality").html(newDivEl);
-        console.log(response) // jdcarra 
-        // air quality colors
-        // 0-50 good-green 
-        // 51-100 moderate-yellow
-        // 101-150 unhealthy for sensitive groups - orange
-        // 151-200 unhealthy - red 
-        // TODO: add if statement block to change div color based on quality 
-    });
+        .then(function (response) {
+            $("#airQuality").empty();
+            var air = $("<p>").text("Air Quality Index (AQI): ");
+            var aqi = $("<span>")
+            aqi.text(response.data.aqi)
+            air.append(aqi)
+            var newDivEl = $('<div>')
+            if (response.data.aqi <= 50 && response.data.aqi >= 0) {
+                aqi.css("background-color", "green")
+                aqi.css("color", "white")
+            } else if (response.data.aqi >= 51 && response.data.aqi <= 100) {
+                aqi.css("background-color", "yellow")
+                aqi.css("color", "black")
+            } else if (response.data.aqi >= 101 && response.data.aqi <= 150) {
+                aqi.css("background-color", "orange")
+                aqi.css("color", "black")
+            } else if (response.data.aqi >= 151 && response.data.aqi <= 200) {
+                aqi.css("background-color", "red")
+                aqi.css("color", "white")
+            }
+
+            newDivEl.append(air);
+            $("#airQuality").html(newDivEl);
+            console.log(response) // jdcarra 
+            // air quality colors
+            // 0-50 good-green 
+            // 51-100 moderate-yellow
+            // 101-150 unhealthy for sensitive groups - orange
+            // 151-200 unhealthy - red 
+            // TODO: add if statement block to change div color based on quality 
+        });
 }
 /* city location logic */
-$("#searchBtn").on("click", function(event) {
+$("#searchBtn").on("click", function (event) {
     event.preventDefault();
     // set and save searched city variable to localstorage //
-    var citySearch = $("#citySearch").val().trim();
-    var savedSearch = $(this).siblings("input").val();
     var saved = [];
-    saved.push(savedSearch);
-    localStorage.setItem('city', JSON.stringify(citySearch));
+    var savedSearches = JSON.parse(localStorage.getItem('city'));
+    if (savedSearches) {
+        console.log(savedSearches);
+        saved.push(...savedSearches);
+    }
+    var citySearch = $("#citySearch").val().trim();
+    citySearch = citySearch.toLowerCase();
+    if (saved.includes(citySearch)) {
+        return;
+    }
+    //var savedSearch = $(this).siblings("input").val();
+    saved.push(citySearch)
+    console.log(saved);
+    localStorage.setItem('city', JSON.stringify(saved));
 
     searchAirQuality(citySearch);
     searchCoord(citySearch);
-    loadSaved();
-});
-/* load localStorage and display on page */
-function loadSaved () {
-    var searchedItem = JSON.parse(localStorage.getItem('city'));
-    var searchHistory = $("<button class='styled-btn btn button is-dark is-outlined is-rounded is-fullwidth'>").text(searchedItem);
+    var searchHistory = $("<button class='styled-btn btn button is-dark is-outlined is-rounded is-fullwidth'>").text(citySearch);
     var divEl = $("<div>");
     divEl.append(searchHistory)
     $("#locationHistory").prepend(divEl);
-    
+});
+/* load localStorage and display on page */
+function loadSaved() {
+    var searchedItems = JSON.parse(localStorage.getItem('city'));
+    if (searchedItems) {
+        for (let index = 0; index < searchedItems.length; index++) {
+            const searchedItem = searchedItems[index];
+            var searchHistory = $("<button class='styled-btn btn button is-dark is-outlined is-rounded is-fullwidth'>").text(searchedItem);
+            var divEl = $("<div>");
+            divEl.append(searchHistory)
+            $("#locationHistory").prepend(divEl);
+        }
+    }
+
 }
 // clickable location history
-$("#locationHistory").on('click', '.btn', function(event) {
+$("#locationHistory").on('click', '.btn', function (event) {
     event.preventDefault();
     searchAirQuality($(this).text());
 });
 // clear location history on clear btn click
-$("#clearBtn").on("click", function(event) {
+$("#clearBtn").on("click", function (event) {
     event.preventDefault();
     $("#locationHistory").html("");
+    localStorage.clear();
 })
+loadSaved();
 
