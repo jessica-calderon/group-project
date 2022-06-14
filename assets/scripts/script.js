05b756aa2bcc54e23d0b89142dd1363cdd09a27b
jessica-calderon <calderonjessica13@yahoo.com>
2022-06-13 21:28:55 -0500

    fix location history click reload divs

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,7 @@
+$(document).ready(function() {
+    $.ajaxSetup({ cache: false });
+  });
+
 // display todays current date on page
 var today = moment().format('MMMM Do, YYYY');
 $("#currentDate").html("<i class='fa-style fa-solid fa-calendar-day'></i>" + today);
